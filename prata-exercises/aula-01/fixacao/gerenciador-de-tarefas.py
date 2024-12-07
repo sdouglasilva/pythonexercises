4f81481ab0cf89ad49a2af87dc65c1068404e317
babicosta <barbaraalexia09@gmail.com>
2024-12-07 13:29:40 -0300

    criando task_manager

--- prata-exercises/aula-01/fixacao/gerenciador-de-tarefas.py
+++ prata-exercises/aula-01/fixacao/gerenciador-de-tarefas.py
@@ -10,3 +10,38 @@
 # Peça ao usuário para marcar uma tarefa como concluída. Ele irá informar o índice de uma tarefa que foi concluída e a remover da lista de tarefas pendentes.
 # Conte quantas tarefas ainda estão pendentes e mostre na tela.
 # Faça um menu com as opções de adicionar, remover, mostrar tarefas pendentes, marcar como concluída e contar quantas tarefas estão pendentes.
+
+def task_list():
+    for i in range(len(tasks)):
+        print(f"[{i}] - {tasks[i]}")
+
+def add_task(task:str,tasks:list):
+    tasks.append(task)
+    
+
+tasks = ['Estudar','Trabalhar','Correr','Preparar Aula']
+
+
+print("*"*10,"|",'TASK MANAGER',"|","*"*48,"")
+print("*"*20,"|",'SEJA BEM VINDO AO GERENCIADOR DE TAREFAS',"|","*"*10,"")
+print("*"*30,"|",'DO',"|","*"*38,"")
+print("*"*40,"|",'DOUGLINHAS',"|","*"*20,"")
+
+
+
+
+
+new_task = input('Digite o nome de uma nova tarefa para adicionar a lista de tarefas.')
+new_task = new_task.capitalize()
+tasks.append(new_task)
+
+print(tasks)
+
+# for i in new_task:
+
+#     print(i)
+
+
+#print(new_task)
+# task_list()
+
